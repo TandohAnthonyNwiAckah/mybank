b2410bf4d7ef4fb7ce42be39eaf1e714e7d5eb33
Tandoh <anthony.tandoh@tanacom.io>
2020-10-27 10:20:50 +0000

    Initial Commit

---
+++ mybank/mybankApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybankApp.swift
+//  mybank
+//
+//  Created by mac on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct mybankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
