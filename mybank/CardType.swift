76e3a47253cecbac89d5b8403d79311f1e070c4d
Tandoh <anthony.tandoh@tanacom.io>
2020-10-27 11:44:47 +0000

    commit Card, CardType ,Expense and TransactionItem to Models

---
+++ mybank/CardType.swift
@@ -0,0 +1,16 @@
+//
+//  CardType.swift
+//  mybank
+//
+//  Created by Tanacom on 10/27/20.
+//
+
+import Foundation
+
+enum CardType:String{
+    
+    case Silver
+    case Gold
+    case Platinum
+    
+}
---
+++ mybank/Expense.swift
@@ -0,0 +1,20 @@
+//
+//  Expense.swift
+//  mybank
+//
+//  Created by Tanacom on 10/27/20.
+//
+
+import Foundation
+
+struct Expense:Identifiable{
+    
+    let id = UUID()
+    let month:String
+    let amount:Float
+    let selected:Bool = false
+    
+    
+    
+    
+}
